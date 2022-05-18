87c091570bfbafcbd9d9483c162c004629a7a4ef
BouqueA <97107038+BouqueA@users.noreply.github.com>
2022-05-18 15:40:31 +0200

    Update and rename ttttttt.js to tttttttt.js

--- tttttttt.js
+++ tttttttt.js
@@ -1,8 +1,5 @@
-$(document).ready(function() {
-    alert("LOAD !");
-});
 var valtest = document.getElementsByName("x_studio_famille_2");
 
 valtest[0].addEventListener('change', (event) => {
-  alert('Val' + event.target.value);
+  console.log(event.target.value);
 });
